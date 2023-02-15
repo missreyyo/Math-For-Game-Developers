22174af51cdadb6f7eafa5e54dbff8fd2fcb3368
missreyyo <reyhanahsen7@gmail.com>
2023-02-15 19:42:44 +0300

    Mesh surface area

--- Assets/Scripts/CrossProductThing.cs
+++ Assets/Scripts/CrossProductThing.cs
@@ -5,8 +5,29 @@ using UnityEditor;
 
 public class CrossProductThing : MonoBehaviour
 {
+    public Mesh mesh;
+    public float area = 0f;
+
+    void OnValidate()
+    {
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        area = 0f;
+        for (int i = 0; i < triangles.Length; i+= 3) {
+            Vector3 a = vertices[triangles[i]];
+            Vector3 b = vertices[triangles[i+1]];
+            Vector3 c = vertices[triangles[i+2]];
+            area += Vector3.Cross(b - a,c-a).magnitude;
+        }
+        area /= 2;
+    }
     void OnDrawGizmos()
     {
+        Vector3[] vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i += 3)
+        {
+            Gizmos.DrawSphere(transform.TransformPoint(vertices[i]), 0.01f);
+        }
         Vector3 headPos = transform.position;
         Vector3 lookDir = transform.forward;
         void DrawRay (Vector3 position, Vector3 direction)  => Handles.DrawAAPolyLine(position, position+direction);
